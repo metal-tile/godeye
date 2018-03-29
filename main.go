ec174836ef205743a74192694afced83faee8be5
sinmetal <metal.tie@gmail.com>
2018-03-29 22:09:29 +0900

    land nodeのname変更 refs #11

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func (ppm *PlayerPositionManager) existActivePlayer() bool {
 }
 
 func main() {
-	err := updateReplicas("default", "land", 0)
+	err := updateReplicas("default", "land-node", 0)
 	if err != nil {
 		fmt.Printf("error:%+v\n", err)
 	}
